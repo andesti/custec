81f43d060f1b34d38a4e62e4bc3033ec1036ef94
Andres Lopez Andrade <arlopeza@andesti.net>
2014-11-13 22:16:54 -0500

    Agregar versión del cliente
    Se agrega la versión del cliente luego de la versión del sistema

--- src/org/compiere/Adempiere.java
+++ src/org/compiere/Adempiere.java
@@ -60,9 +60,9 @@ public final class Adempiere
 	/** Main Version String         */
 	// Conventions for naming second number is even for stable, and odd for unstable
 	// the releases will have a suffix (a) for alpha - (b) for beta - (t) for trunk - (s) for stable - and (LTS) for long term support
-	static public String	MAIN_VERSION	= "Release 3.7.0LTS";
+	static public String	MAIN_VERSION	= "Versión 3.7.0LTS";
 	/** Detail Version as date      Used for Client/Server		*/
-	static public String	DATE_VERSION	= "2011-09-01";
+	static public String	DATE_VERSION	= "Cliente 3.7.3";
 	/** Database Version as date    Compared with AD_System		*/
 	static public String	DB_VERSION		= "2011-09-01";
 
